e342250cd5dc7dbbc8bad065601fd1bf0fdea102
Robbie W. <robbiew@gmail.com>
2026-06-25 11:09:06 -0700

    bump version

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,4 +3,4 @@ package version
 // Number is the global ViSiON/3 version.
 // Set this at build time with:
 // -ldflags "-X github.com/ViSiON-3/vision-3-bbs/internal/version.Number=1.0.0"
-var Number = "0.1.0"
+var Number = "0.1.1"
